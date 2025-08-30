574d98ceaa98fe8edd3cd746e188e0cc83359240
yuvrajparmar1605 <157046929+yuvrajparmar1605@users.noreply.github.com>
2025-08-30 22:54:26 +0530

    Convert backend to ESM and update package.json

--- models/User.js
+++ models/User.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
+
+export default User;
